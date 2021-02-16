ed25570a14e9ddeac03601d4d00c567899e1229e
Sadki Khadija <Sadki.Khadija@student.intecbrussel.be>
2021-02-16 11:57:43 +0100

    Add project files.

---
+++ DelegatesSimple/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesSimple
+{
+   // public delegate void AddDelegate(int x, int y);
+   // public delegate string GreetingsDelegate(string str);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  AddDelegate addDelegate = new AddDelegate(AddNumbers);
+            // addDelegate(3, 2);
+            // addDelegate.Invoke(3, 2); // hadi tari9a akhra blast lawla kandiroha
+
+            Action<int, int> addDelegate = AddNumbers;
+            addDelegate(3, 2);
+
+
+
+            //AddDelegate subtract = new AddDelegate(Subtract);
+            // subtract(8, 2);
+            //tari9a akhra 
+            Action<int, int> subtract = Subtract;
+            subtract(8, 2);
+
+
+
+           // GreetingsDelegate greetingsDelegate = new GreetingsDelegate(Greetings);
+           // string result = greetingsDelegate("Hello c# Group");
+            // string result = greetingsDelegate("Hello c# Group"); hta hadi tari9a akhra
+            Func<string,string> greetingsDelegate = Greetings;
+            string result = greetingsDelegate("Hello c# Group");
+
+
+            Console.WriteLine(result);
+
+           // GreetingsDelegate sayHello = new GreetingsDelegate(SayHello);
+           // string sayHelloResult = sayHello("hello");
+            Console.WriteLine(sayHelloresult);
+        }
+        //static Method 
+        public static void AddNumbers(int a, int b)
+        {
+            Console.WriteLine($"the sum of {a} and {b} is {a+b}");
+
+        }  
+        public static void Subtract(int a, int b)
+        {
+            Console.WriteLine($"The substraction of {a} and {b} is {a/b}");
+        }
+         public static string Greetings(string str)
+        {
+            return str;
+        }
+        public static string SayHello(string str)
+        {
+            return str;
+        }
+    }
+}
---
+++ MultiCastDelegate/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiCastDelegate
+{
+  //awal haja kandiroha hia hadi kankatbo hnaya
+    public delegate void CalculateDelegate(int n1, int n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kansaliw ma 3 kanjiw hnaya kankatbo i kan instancemaken in delegate
+            CalculateDelegate del1 = new CalculateDelegate(Add);
+            CalculateDelegate del2 = new CalculateDelegate(Substract);
+            CalculateDelegate del3 = new CalculateDelegate(Multiply);
+            CalculateDelegate del4 = new CalculateDelegate(Divide);
+
+            //add 4 delegate to del5 which is multicast delegate
+            CalculateDelegate del5 = del1 + del2 + del3 + del4;
+            del5.Invoke(20, 5);
+
+            del5 -= del2;
+            Console.WriteLine("----------------------------");
+            Console.WriteLine();
+            del5.Invoke(10, 2);
+
+            Console.WriteLine("----------------------------");
+
+            del5 += del2;
+            del5 += del2;
+            del5 += del2;
+            del5.Invoke(6, 3);
+
+
+
+        }
+
+        // hna tania dar static waheda jdida
+        public static void Add(int x, int y)
+        {
+            Console.WriteLine($"the Sum of {x} and {y} is {x+y}");
+        }
+
+        public static void Substract (int a, int b)
+        {
+            Console.WriteLine($"the Substraction of {a} and {b} is {a-b}");
+        }
+        //hadi 3 kankatboha hnaya
+        public static void Multiply(int m1, int m2)
+        {
+            Console.WriteLine($"the Multiplication of {m1} and {m2} is {m1*m2}");
+
+        }
+        // hadi 4
+        public static void Divide(int d1, int d2)
+        {
+            Console.WriteLine($"the Division of {d1} and {d2} is {d1 * d2}");
+
+        }
+
+    }
+}
